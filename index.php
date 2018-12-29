25153f197dadfcd4c16370d3b9323ee0bc592652
belindal <26785408+belindal@users.noreply.github.com>
2018-12-28 19:47:49 -0800

    added content

--- index.php
+++ index.php
@@ -1,24 +1,55 @@
-<html>
- <head>
-  <title>Belinda Li</title>
- </head>
- <body>
- <h1> Belinda Li </h1>
- <p>This site is pretty barren right now. I'll add more content (and make it prettier!) when I have time :).</p>
- <h2> Intro </h2>
- <p>I am currently a senior studying computer science at the University of Washington. My research interests include natural language processing and machine learning.</p>
- <p>This summer, I interned at Facebook and developed Facebook's first in-house bot detection solution.</p>
- <p>Outside of computer science, I dance <a href="http://hengdadance.com/">ballet and Chinese classical dance</a>.</p>
- <p>I also <a href="https://robinsoncenter.uw.edu/programs/eep/">started college at the age of 15</a>.</p>
+<html lang='en'>
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Belinda Li</title>
+    <link rel='stylesheet' href='style.css' />
+  </head>
+  <body>
+    <header>
+      <h1 id='name'> BELINDA LI </h1>
+    <header>
+    <figure>
+      <a href='yellowstone.jpg'><img src='yellowstone_cropped.png' alt='Taken at Yellowstone National Park, Wyoming, USA' width=400 /></a>
+      <figcaption>Taken at Yellowstone National Park</figcaption>
+    </figure>
+    <p>This site is pretty barren right now. I&rsquo;ll add more content (and make it prettier!) when I have time :).</p>
+    <hr />
+    <h2> Intro </h2>
+    <p>I am currently a senior studying computer science at the University of Washington (heretofore abbreviated UW).
+       I started college at the age of 15 through the <a href='https://robinsoncenter.uw.edu/programs/eep/' target='_blank'>Robinson Center&rsquo;s Early Entrance Program</a>, a program where students attend college full-time after middle school (i.e. 8th grade), following just a year of &ldquo;Transition School.&rdquo;
+       I&rsquo;ve also taken a ton of chemistry/organic chemistry/biology classes my freshman and sophomore years when I was wavering between computer science and pre-med.
+       But alas, in the end I chose computer science. 
+    <p>My current research interests include natural language processing and machine learning. Refer below for projects I&rsquo;ve worked on.</p>
+    <p>This summer, I interned at Facebook and developed Facebook&rsquo;s first in-house biometric bot detection solution.
+       I wrote a script to track and store user mouse movements on the facebook.com website
+       (yes, you heard that right--I implemented yet <em>another</em> way for Facebook to collect user data.
+        If it helps alleviate your fears though, know that your mouse movements are only being collected for the first two minutes you open the site, and is, as far as I know, only being used for bot detection).
+       I also created a visualizer for these collected mouse movements and implemented bot detection techniques on these mouse movements.
+       To learn more about how I used mouse movements to perform bot detection during my internship, click here.
+       <!-- TODO: add link here -->
+    </p>
+    <p>Outside of school/computer science, I dance ballet and Chinese classical dance at UW and at <a href='http://hengdadance.com/' target='_blank'>Hengda Dance Academy</a>.
+       I started dancing at age 4 (although at that age, can that really count as dancing?).
+       Then, for 3 years starting at age 8, I attended Pacific Northwest Ballet School, one of the top ballet schools in the country.
+       Afterwards, I took classes at Hengda Dance Academy.
+    </p>
+    <p>
+      In my free time (though perhaps better phrased as &ldquo;in the time I waste when I should be doing more productive tasks&rdquo;), I browse political news.
+      I enjoy the statistical analyses on <a href='https://fivethirtyeight.com/' target='_blank'>FiveThirtyEight</a>.
+    </p>
 
- <h2> Research </h2>
- <p>Document-Level Entity to Entity Sentiment Analysis with LSTM-based Models</p>
- <p>[<a href="./ent2ent_sentiment_2018.pdf">Paper</a>]
-    [<a href="./ent2ent_sentiment_2018.poster.pdf">Poster</a>]</p>
+    <hr />
+    <h2> Research </h2>
+    <p>Document-Level Entity to Entity Sentiment Analysis with LSTM-based Models</p>
+    <p>[<a href='./ent2ent_sentiment_2018.pdf' target='_blank'>Paper</a>]
+       [<a href='./ent2ent_sentiment_2018.poster.pdf' target='_blank'>Poster</a>]</p>
 
- <h2> Miscellanious </h2>
- <p><a href="./resume_belinda.pdf">Resume</a></p>
- <p><a href="red">Autobiography of Red</a> (English 200 Assignment)</p>
- <p><a href="cmu_lti_video.mp4">Graduate School Application Video (CMU LTI Program)</a>
- </body>
+    <hr /> 
+    <h2> Miscellanious </h2>
+    <ul>
+      <li><a href='./resume_belinda.pdf' target='_blank'>Resume</a></li>
+      <li><a href='red' target='_blank'>Autobiography of Red</a> (English 200 Assignment)</li>
+      <li><a href='cmu_lti_video.mp4' target='_blank'>Graduate School Application Video (CMU LTI Program)</li>
+    </ul>
+  </body>
 </html>
