ca0bac52b0842c0784711faea6ad841a3a105aac
Belinda Li <26785408+belindal@users.noreply.github.com>
2019-12-10 10:20:39 -0800

    more info

---
+++ dance/index.php
@@ -0,0 +1,66 @@
+<html lang='en'>
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Belinda Li</title>
+    <!--<link rel='stylesheet' href='./styles/base.css' />-->
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133669415-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'UA-133669415-1');
+      </script>
+  </head>
+  <body>
+    <h1>Dance Pictures</h1>
+    <p>Click any picture to expand.</p>
+    <h2>2019</h2>
+    <h3>Arabesque at UW graduation</h3>
+    <figure>
+      <a href='IMG_0186.JPG'><img src='IMG_0186.JPG' width=700 /></a>
+    </figure>
+
+    <h3>扇舞丹青 [Chinese Classical Dance]</h3>
+    <figure>
+      <a href='shan_wu_dan_qin.jpg'><img src='shan_wu_dan_qin.jpg' width=500 /></a>
+    </figure>
+
+    <h2>2018</h2>
+    <h3>Giselle [Ballet]</h3>
+    <figure>
+      <a href='ballet_1.png'><img src='ballet_1.png' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4484.jpg'><img src='20180520-4484.jpg' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4494.jpg'><img src='20180520-4494.jpg' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4501.jpg'><img src='20180520-4501.jpg' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4506.jpg'><img src='20180520-4506.jpg' width=500 /></a>
+    </figure>
+    <h3>贵妃醉酒 [Chinese Classical Dance]</h3>
+    <figure>
+      <a href='20180520-4217.jpg'><img src='20180520-4217.jpg' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4299.jpg'><img src='20180520-4299.jpg' width=500 /></a>
+    </figure>
+    <figure>
+      <a href='20180520-4351.jpg'><img src='20180520-4351.jpg' width=500 /></a>
+    </figure>
+
+    <h2>2014</h2>
+    <h3>Swan Lake [Ballet]</h3>
+    <p>I am Odette (middle) here.</p>
+    <figure>
+      <a href='4172697_2.jpeg'><img src='4172697_2.jpeg' height=500 /></a>
+    </figure>
+
+  </body>
+</html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset='UTF-8'/>
     <title>Belinda Li</title>
-    <link rel='stylesheet' href='./styles/base.css' />
+    <!--<link rel='stylesheet' href='./styles/base.css' />-->
       <!-- Global site tag (gtag.js) - Google Analytics -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133669415-1"></script>
       <script>
@@ -18,18 +18,36 @@
       <h1> BELINDA LI </h1>
     <header>
     <figure>
-      <a href='yellowstone.jpg'><img src='yellowstone_cropped.png' alt='Taken at Yellowstone National Park, Wyoming, USA' width=200 /></a>
-      <figcaption>Taken at Yellowstone National Park (click to expand)</figcaption>
+      <a href='main.jpg'><img src='main_cropped.png' width=300 /></a>
     </figure>
+
+    <hr />
     <h2> Intro </h2>
-    <p>I currently work at Facebook as a software engineer. Previously, I got my bachelors in computer science from the University of Washington. I entered college 3 years early through the <a href='https://robinsoncenter.uw.edu/programs/eep/' target='_blank'>early entrance program</a>.</p> <!--Before that, I got my GED through Robinson Center's <a href='https://robinsoncenter.uw.edu/programs/eep/' target='_blank'>transition school program</a>.</p>--> 
-    <p>Within computer science my main interest is natural language processing, a subset of machine learning that focuses on language. My fascination with language originally stemmed from a book I read in Transition School English--Catherine Belsey's <em>Critical Practice</em>. To this date, I still think it's one of the most fascinating books I've read. It's a very dense English/linguistic philosophy book that goes into lengthy lectures about post-Sassurean philosophy (Barthes, Derrida, Foucault, etc.). I won't claim to have understood it 100%, but regardless, it led to my current adament belief that language is absolutely foundational to our society (a la Catherine Belsey, language defines our social ideologies, is defined /by/ our social ideologies, and the meaning derived from text is a reflection of the social structures at the time and place of its composition--even more than it is a reflection of writer or reader intent).</p>
+    <p>I currently work at Facebook AI Applied Research, where I solve AI problems that lie at the intersection of research and product.
+    Previously, I got my bachelors in computer science from the University of Washington, where I did nearly 2 years of undergraduate research in Natural Language Processing (NLP).
+    My undergrad advisor was <a href='https://www.cs.washington.edu/people/faculty/lsz' target='_blank'>Luke Zettlemoyer</a>.
+    Before UW, I went to &ldquo;high school&rdquo; through the <a href='https://robinsoncenter.uw.edu/programs/eep/' target='_blank'>Robinson Center&rsquo;s transition school to early entrance program</a>.</p>
+    <!--Before that, I got my GED through Robinson Center's <a href='https://robinsoncenter.uw.edu/programs/eep/' target='_blank'>transition school program</a>.</p>--> 
+    <p>Within computer science my main interest is natural language processing, a subset of machine learning that focuses on language.
+    My fascination with language originally stemmed from a book I read in Transition School English--Catherine Belsey&rsquo;s <em>Critical Practice</em>.
+    <!-- To this date, I still think it&rsquo;s one of the most fascinating books I&rsquo;ve read, and one which changed my entire outlook on language. -->
+    It&rsquo;s a fairly dense English/linguistic philosophy textbook that goes into depth on post-structural and post-Sassurean theory (think Barthes, Derrida, possibly even Foucault).
+    I won&rsquo;t claim to have understood it 100%, but regardless, the book changed my entire outlook and understanding of language.
+    It led to my current adamant belief that language is absolutely foundational to our society--a la Catherine Belsey, language defines our social ideologies, is defined /by/ our social ideologies, and the meaning derived from text is moreso a reflection of the social structures at the time and place of its composition than it is a reflection of writer or reader intent.</p>
     
-    <p>Thus, I believe so-called "teaching machines to read and write" is probably one of the most intriguing, difficult, and potentially problematic (if we are not careful) challenges of the 21st century. I also believe will be <em>the</em> main driving force towards artificial general intelligence, if we will ever able to achieve that.</p>
+    <p>Thus, I believe so-called &ldquo;teaching machines to read and write&rdquo; is probably one of the most intriguing, difficult, and potentially problematic (if we are not careful) challenges of the 21st century. I also believe will be <em>the</em> main driving force towards artificial general intelligence (if we will ever able to achieve that).</p>
 
-    <p>But enough abstract futuristic talk, some specific areas and problems I'm interested in include: natural language understanding and generation, conversational AI, question-answering, fake news detection, social bias in machine learning, transfer learning, scaling large models, model interpretatibility, etc. It's a long list, but even this list is by no means exhaustive--just a taste of the interesting work I've been exposed to during my undergrad and time at Facebook.</p>
+    <p>But enough abstract futuristic talk, some specific problems I&rsquo;m interested in include:
+    <ul>
+      <li> Generalizability: can we make NLP models extrapolate to arbitrary, possibly unseen, tasks? </li>
+      <li> Interpretability: current deep learning models, esp. state-of-the-art ones in NLP (i.e. BERT) are often described as &ldquo;black boxes.&rdquo; Can we make them more transparent? And more importantly, without sacrificing accuracy? I believe interpretability will be key in driving our field forward. </li>
+      <li> Scalability and resource efficiency: current models are extremely costly to train, and have significant downstream environmental and economic impacts. How can we mitigate these issue? </li>
+      <li> AI for social good: reducing social bias in NLP models (see above paragraphs), misinformation or fake news detection, etc. </li>
+    </ul>
+    <!-- natural language understanding and generation, conversational AI, question-answering, fake news detection, social bias in machine learning, transfer learning, scaling large models, model interpretatibility, etc.-->
+    This list is by no means comprehensive--just a taste of the interesting work I&rsquo;ve been exposed to during my undergrad and time at Facebook.</p>
 
-    <p>Anyways, there's still a lot for me to learn! I recently started an NLP blog <a href='blog/index.php'>here</a> to help myself as well as anyone else who's interested get a better grasp on the literature in the field. Feel free to check it out!</p>
+    <!--<p>Anyways, there's still a lot for me to learn! I recently started an NLP blog <a href='blog/index.php'>here</a> to help myself as well as anyone else who's interested get a better grasp on the literature in the field. Feel free to check it out!</p> -->
     <!--
     <p>My primary research interest is in the field of natural language processing. Refer below for projects I&rsquo;ve worked on.</p> -->
     <!--
@@ -40,23 +58,40 @@
        I implemented bot detection techniques on these mouse movements.
        To learn more about how I used mouse movements to perform bot detection during my internship, click here. -->
        <!-- TODO: add link here -->
-    <p>Outside of work and computer science, I dance ballet and Chinese classical dance at <a href='http://hengdadance.com/' target='_blank'>Hengda Dance Academy</a>. (Some pictures <a href='dance/index.php'>here</a>). I also play the piano. 
+    <hr />
+
+    <h2> Research </h2>
+    <p>Contact me personally for copies of recent papers :).</p>
+    <!--<p>Discrete Annotation for Active Learning for Coreference Resolution</p>-->
+    <!-- <p><em>Belinda Li</em>. Document-Level Entity to Entity Sentiment Analysis with LSTM-based Models. 2018. <em>(CSE 481N Capstone Project)</em><br>
+       [<a href='./papers/ent2ent_sentiment_2018.pdf' target='_blank'>Paper</a>]
+       [<a href='./papers/ent2ent_sentiment_2018.poster.pdf' target='_blank'>Poster</a>]</p> -->
+
+    <hr /> 
+    <h2>Extracurriculars</h2>
+    <p>Outside of work and computer science, I dance ballet and Chinese classical dance at <a href='http://hengdadance.com/' target='_blank'>Hengda Dance Academy</a>.</p>
+    <figure>
+      <a href='dance/IMG_0186.JPG'><img src='dance/IMG_0186.JPG' width=500 /></a>
+    </figure>
+    <p>(More pictures <a href='dance/index.php'>here</a>).</p>
+    <p>I also play the piano.</p> 
 <!--       I started dancing at age 4 (although at that age, can that really count as dancing?).
        Then, for 3 years starting at age 8, I attended Pacific Northwest Ballet School, one of the top ballet schools in the country.
        Afterwards, I took classes at Hengda Dance Academy. -->
     </p>
-    <p>I'm also very interested in politics and social issues. In my free time, I follow political news as well as statistical analyses like <a href='https://fivethirtyeight.com/' target='_blank'>FiveThirtyEight</a>. As they have not yet released their 2020 model, I am working on creating my own polling aggregation model for the the 2020 Democratic presidential primaries.
+    <p>I&rsquo;m also very interested in politics and social issues. In my free time, I follow political news as well as statistical analyses like <a href='https://fivethirtyeight.com/' target='_blank'>FiveThirtyEight</a>. I&rsquo;m (cautiously) looking forward to the 2020 elections.
+    <!-- As they have not yet released their 2020 model, I am working on creating my own polling aggregation model for the the 2020 Democratic presidential primaries. -->
     </p>
-    <p>You can check out my <a href='./resume_belinda.pdf' target='_blank'>resume</a> here. [Disclaimer: may be out-of-date].</p>
+    <!-- <p>You can check out my <a href='./resume_belinda.pdf' target='_blank'>resume</a> here. [Disclaimer: probably out-of-date].</p> -->
+
+   
+    <hr /> 
+    <h2> Contact </h2>
+    <p>Email: <a href = "mailto: belindali@fb.com">belindali@fb.com</a> // <a href = "mailto: belindazli65@gmail.com">belindazli65@gmail.com</a> </p>
+    <!-- <p>Github: <a href = "https://github.com/belindal">belindal</a></p> -->
+    <p>Twitter: <a href = "https://twitter.com/belinda_nlp">belinda_nlp</a></p>
 
     <hr />
-    <h2> Research </h2>
-    <p>Discrete Annotation for Active Learning for Coreference Resolution</p>
-    <p>Document-Level Entity to Entity Sentiment Analysis with LSTM-based Models <br>
-       [<a href='./papers/ent2ent_sentiment_2018.pdf' target='_blank'>Paper</a>]
-       [<a href='./papers/ent2ent_sentiment_2018.poster.pdf' target='_blank'>Poster</a>]</p>
-
-    <hr /> 
     <!--<h2> Miscellanious </h2>
     <a href='red' target='_blank'>Autobiography of Red</a> (English 200 Assignment)
       li><a href='cmu_lti_video.mp4' target='_blank'>Graduate School Application Video (CMU LTI Program)</li> -->
