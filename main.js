00110e34c6346a2197b48d4f5559442f379197c2
belindali <26785408+belindal@users.noreply.github.com>
2020-06-13 18:35:39 -0700

    scholar link

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function show_email(id) {
   if (document.getElementById(id).innerHTML === "") {
-    document.getElementById(id).innerHTML = "bzl [at] mit.edu";
-    document.getElementById(id).innerHTML += "&emsp;OR&emsp;";
-    document.getElementById(id).innerHTML += "belindali [at] fb.com";
-    document.getElementById(id).innerHTML += "&emsp;OR&emsp;";
+    document.getElementById(id).innerHTML = "bzl [at] mit.edu\n";
+    document.getElementById(id).innerHTML += "&emsp;OR&emsp;\n";
+    document.getElementById(id).innerHTML += "belindali [at] fb.com\n";
+    document.getElementById(id).innerHTML += "&emsp;OR&emsp;\n";
     document.getElementById(id).innerHTML += "belindazli65 [at] gmail.com";
   } else {
     document.getElementById(id).innerHTML = "";
